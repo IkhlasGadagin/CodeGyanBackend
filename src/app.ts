3c859bb9a8427400b5da6d3b47f0af3ee610c4c2
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-05-03 01:25:43 +0530

    the http-error is hamdled and the middleware is understood

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
+
+
+    const error = createHttpError("Something went wrong");
+    throw error;
+
     res.json({
-        message: "Welcome to the Coders Gyan API!"
+        message: "Welcome to the Coders Gyan API!" });
+});
+
+
+//Globat Error declaration
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.status || 500;
+    res.status(statusCode).json({
+        message: err.message || "Internal Server Error",
+        errorStack: config.env === 'development' ? err.stack : ""
     });
 });
 
