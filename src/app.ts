9636f865ed9e495cb14e242623f787055ffc6a7d
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-05-03 01:41:04 +0530

    the middleware for the globalError is separately written

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import express, { NextFunction, Request, Response } from "express";
-import createHttpError, { HttpError } from "http-errors";
-import { config } from "./config/config";
-
+import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+// import createHttpError from "http-errors";
 const app = express();
 
 app.get("/", (req, res, next) => {
 
 
-    const error = createHttpError("Something went wrong");
-    throw error;
+    // const error = createHttpError("Something went wrong");
+    // throw error;
 
     res.json({
         message: "Welcome to the Coders Gyan API!" });
@@ -16,12 +15,6 @@ app.get("/", (req, res, next) => {
 
 
 //Globat Error declaration
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.status || 500;
-    res.status(statusCode).json({
-        message: err.message || "Internal Server Error",
-        errorStack: config.env === 'development' ? err.stack : ""
-    });
-});
+app.use(globalErrorHandler);
 
 export default app;
