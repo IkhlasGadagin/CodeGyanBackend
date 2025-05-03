dcde650801425ad5cf8560c2a83afb9288767db1
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-05-04 01:44:42 +0530

    add try catch over the db and the token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,29 +4,47 @@ import User from "./userModel";
 import bcrypt from "bcrypt"
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User as UserType } from "./userTypes";
 
-const registerUser = async (req: Request, res: Response,next: NextFunction) => {
-    const {username, email, password} = req.body;
+const registerUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { username, email, password } = req.body;
 
-    if(!username || !email || !password){
+    if (!username || !email || !password) {
         const error = createHttpError(400, "All fields are required");
         return next(error);
     }
-    const user = await User.findOne({email})
-    if (user) {
-        const error = createHttpError(400, "User already exists with this email");
-        return next(error);
+
+    try {
+        const user = await User.findOne({ email })
+        if (user) {
+            const error = createHttpError(400, "User already exists with this email");
+            return next(error);
+        }
+    } catch (error) {
+        return next(createHttpError(500, "Error while getting user"))
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const usercreated = await User.create({username, email, password:hashedPassword});
+    let newUser: UserType;
+    try {
+
+        newUser = await User.create({ username, email, password: hashedPassword });
+    } catch (error) {
+        return next(createHttpError(500, "Error while creating user"))
+
+    }
 
 
-    const token = sign({userId: usercreated._id},config.jwtSecrete as string, {expiresIn: "7d"});
-    res.json({
-        accessToken: token,
-        message: "User registered successfully Ikhlas"
-    });
+    try {
+        const token = sign({ userId: newUser._id }, config.jwtSecrete as string, { expiresIn: "7d" });
+        res.json({
+            accessToken: token,
+            message: "User registered successfully Ikhlas"
+        });
+    } catch (error) {
+        return next(createHttpError(500, "Error while generating token"))
+    }
 }
 
-export  {registerUser};
+export { registerUser };
