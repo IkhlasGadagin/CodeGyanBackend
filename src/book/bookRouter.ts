0271ac8ef70c024b43e3e55d327794deb47be7b0
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-05-04 15:32:40 +0530

    the multer is set up

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import { createBook, getAllBooks, getBookById, updateBook, deleteBook } from "./bookController";
+import path from "node:path";
+import multer from "multer";
 
 const bookRouter = express.Router();
+
+const upload = multer({ 
+    dest:path.resolve(__dirname, '../../public/data/uploads'),
+    limits: {fileSize: 3e7},
+    // storage: multer.memoryStorage() 
+
+});
 
 
 bookRouter.post("/create", createBook);
