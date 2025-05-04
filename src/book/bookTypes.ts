b007b2f12fbf5ef477ca370cc9b941515b58ec92
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-05-04 14:56:25 +0530

    the interface of the Book is created

---
+++ src/book/bookTypes.ts
@@ -0,0 +1,14 @@
+import User from "../user/userModel";
+
+export interface IBook {
+    _id: string;
+    title: string;
+    author: User;
+    genre: string;
+    coverImage: string;
+    file: string;
+    createdAt: Date;
+    updatedAt: Date;
+    description: string;
+}
+
