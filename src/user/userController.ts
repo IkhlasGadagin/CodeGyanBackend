788647099d416c577723c175cb344a494e83d9d6
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-05-04 12:38:39 +0530

    the login api with data recieving

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt"
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 import { User as UserType } from "./userTypes";
+import { log } from "console";
 
 const registerUser = async (req: Request, res: Response, next: NextFunction) => {
     const { username, email, password } = req.body;
@@ -47,4 +48,48 @@ const registerUser = async (req: Request, res: Response, next: NextFunction) => 
     }
 }
 
-export { registerUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+
+    res.json({
+        message: "User logged in successfully"
+    })
+    const { email, password } = req.body;
+    console.log(email, password);
+   /*  
+
+    if (!email || !password) {
+        const error = createHttpError(400, "emain and password is required");
+        return next(error);
+    }
+
+    let currentuser =UserType
+    try {
+        const currentuser = await User.findOne({ email });
+        if (!currentuser) {
+            const error = createHttpError(400, "User not found with this email");
+            return next(error);
+        }
+    } catch (error) {
+        return next(createHttpError(500, "Error while getting user"))
+    }
+    
+    const isPasswordValid = await bcrypt.compare(password, password);
+    if (!isPasswordValid) {
+        const error = createHttpError(400, "Invalid password");
+        return next(error);
+    }
+
+    try {
+        const token = sign({ userId: user._id }, config.jwtSecrete as string, { expiresIn: "7d" });
+        res.json({
+            accessToken: token,
+            message: "User logged in successfully"
+        });
+    } catch (error) {
+        return next(createHttpError(500, "Error while generating token"))
+    } */
+}
+
+
+
+export { registerUser, loginUser };
