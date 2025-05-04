11ff99cd2be4768b8baa888c3bd71887777312dd
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-05-04 13:26:24 +0530

    the register and login is saving in db

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,7 +5,6 @@ import bcrypt from "bcrypt"
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 import { User as UserType } from "./userTypes";
-import { log } from "console";
 
 const registerUser = async (req: Request, res: Response, next: NextFunction) => {
     const { username, email, password } = req.body;
@@ -50,32 +49,28 @@ const registerUser = async (req: Request, res: Response, next: NextFunction) => 
 
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
 
-    res.json({
-        message: "User logged in successfully"
-    })
+
     const { email, password } = req.body;
     console.log(email, password);
-   /*  
+
 
     if (!email || !password) {
-        const error = createHttpError(400, "emain and password is required");
+        const error = createHttpError(400, "email and password is required");
         return next(error);
     }
 
-    let currentuser =UserType
+    const user = await User.findOne({ email });
     try {
-        const currentuser = await User.findOne({ email });
-        if (!currentuser) {
-            const error = createHttpError(400, "User not found with this email");
+        if (!user) {
+            const error = createHttpError(404, "User not found with this email");
             return next(error);
         }
     } catch (error) {
         return next(createHttpError(500, "Error while getting user"))
     }
-    
-    const isPasswordValid = await bcrypt.compare(password, password);
-    if (!isPasswordValid) {
-        const error = createHttpError(400, "Invalid password");
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        const error = createHttpError(400, "Invalid password for this email");
         return next(error);
     }
 
@@ -83,11 +78,41 @@ const loginUser = async (req: Request, res: Response, next: NextFunction) => {
         const token = sign({ userId: user._id }, config.jwtSecrete as string, { expiresIn: "7d" });
         res.json({
             accessToken: token,
+            // userId: user._id,
             message: "User logged in successfully"
         });
     } catch (error) {
         return next(createHttpError(500, "Error while generating token"))
-    } */
+    }
+
+
+    /* 
+       let currentuser =UserType
+       try {
+           const currentuser = await User.findOne({ email });
+           if (!currentuser) {
+               const error = createHttpError(400, "User not found with this email");
+               return next(error);
+           }
+       } catch (error) {
+           return next(createHttpError(500, "Error while getting user"))
+       }
+       
+       const isPasswordValid = await bcrypt.compare(password, password);
+       if (!isPasswordValid) {
+           const error = createHttpError(400, "Invalid password");
+           return next(error);
+       }
+   
+       try {
+           const token = sign({ userId: user._id }, config.jwtSecrete as string, { expiresIn: "7d" });
+           res.json({
+               accessToken: token,
+               message: "User logged in successfully"
+           });
+       } catch (error) {
+           return next(createHttpError(500, "Error while generating token"))
+       } */
 }
 
 
